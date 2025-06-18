70305ac94d60e7737e16df2ab149623e1d3ea11e
Leonardo Montalvo <leomx329@gmail.com>
2025-06-18 18:12:11 +0200

    creando el proyecto

---
+++ crud-cliente/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace crud_cliente.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
