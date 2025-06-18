640803248b87510bc0de001fc0462cee61a6d587
Leonardo Montalvo <leomx329@gmail.com>
2025-06-18 19:57:06 +0200

    crud parte II

--- crud-cliente/Controllers/ClienteController.cs
+++ crud-cliente/Controllers/ClienteController.cs
@@ -19,5 +19,77 @@ namespace crud_cliente.Controllers
             IEnumerable<Cliente> lista = _db.Cliente;
             return View(lista);
         }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Crear(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Cliente.Add(cliente);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public IActionResult Editar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Cliente.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Editar(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Cliente.Update(cliente);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(cliente);
+        }
+
+        public IActionResult Eliminar(int? Id)
+        {
+            if (Id == null || Id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Cliente.Find(Id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Eliminar(Cliente cliente)
+        {
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+            _db.Cliente.Remove(cliente);
+            _db.SaveChanges(); 
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- crud-cliente/Models/Cliente.cs
+++ crud-cliente/Models/Cliente.cs
@@ -8,14 +8,19 @@ namespace crud_cliente.Models
         [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "El Nombre es Obligatorio.")]
         public string Nombre { get; set; }
 
+        [Required(ErrorMessage = "El Tipo de Documento es Obligatorio.")]
         public string TipoDocumento { get; set; }
 
+        [Required(ErrorMessage = "El Documento es Obligatorio.")]
         public string Documento { get; set; }
 
+        [Required(ErrorMessage = "El Celular es Obligatorio.")]
         public string Celular {  get; set; }
 
+        [Required(ErrorMessage = "La Direccion es Obligatoria.")]
         public string Direccion {  get; set; }
     }
 }
